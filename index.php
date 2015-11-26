5c23dd4df73a65c4d741c685ca67169ceb6a74c7
Oscar <ciw3023474h@yahoo.com>
2015-11-26 14:35:35 +0000

    Create index.php

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+//error reporting
+error_reporting(E_ALL);
+ini_set("display_errors", "On");
+
+?>
+
+<!doctype html>
+
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Project Test</title>
+	<meta name="description" content="this is project test">
+	<meta name="keywords" content="test, project">
+	<meta http-equiv="imagetoolbar" content="no">
+	<meta name="MSSmartTagsPreventParsing" content="true">
+	<meta name="author" content="Oscar Edmond for Octal Data Solutions">
+    <meta name="robots" content="noidex,follow" />
+    <meta http-equiv="revisit-after" content="1 day">
+    <meta name="rating" content="General">
+    <meta name="rating" content="safe for kids">
+	<meta http-equiv="X-UA-Compatible" content="IE=11" />
+	<meta name="viewport" content="width=device-width, initial-scale= 1.0,maximum-scale= 3"/>
+	<link rel="alternate" hreflang="x-default" href="#" />
+	<link rel="alternate" hreflang="en-GB" href="#" />
+	
+	
+	<link href='https://fonts.googleapis.com/css?family=PT+Sans' rel='stylesheet' type='text/css'>
+	<link href='https://fonts.googleapis.com/css?family=Courgette' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" href="./script/project.css" media="screen and (min-width:680px)"/>
+	
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script async src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" ></script>
+     <script async type='text/javascript' src='./script/project.js'></script>
+</head>
+<body>
+<section class="wrapper">
+<main>
+<input type="text" id="search" name="search" class="search" placeholder="search">
+<button type="submit" id="submit">Search</button>
+<section id="output"></section>
+
+
+
+
+</main>
+<footer></footer>
+</section>
+</body>
+</html>
